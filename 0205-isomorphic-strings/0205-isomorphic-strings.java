bfc5664fdceb73929500c0277da52b03b30b1961
Prachisoni123-hub <prachiii0987@gmail.com>
2026-04-07 01:14:12 +0530

    Time: 17 ms (38.8%), Space: 43.8 MB (65.11%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,27 +1,30 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        char[] arr= new char[128];// null characters
-        for(int i=0;i<s.length();i++){
-        char ch = s.charAt(i);
-         char dh = t.charAt(i);
-         int idx = (int)ch;
-         
-         if (arr[idx]=='\0') arr[idx]=dh;
-         else{
-            if(arr[idx]!=dh) return false;
-         }
-      }
-        char[] a= new char[128];// null characters
-        for(int i=0;i<t.length();i++){
-          char ch = t.charAt(i);
-          char dh = s.charAt(i);
-           int idx = (int)ch;
-         
-         if (a[idx]=='\0') a[idx]=dh;
-         else{
-            if(a[idx]!=dh) return false;
-         }
-      }
-      return true;
+        if(s.length()!= t.length())return false;
+      HashMap<Character, Character> mapST = new HashMap<>();
+        HashMap<Character, Character> mapTS = new HashMap<>();
+
+        // iterating through whole loop taking any length
+
+        for(int i =0;i<s.length() ;i++){
+            // picking characters
+            char char1 = s.charAt(i);
+            char char2 = t.charAt(i);
+
+// start comparing from s --> t
+if(mapST.containsKey(char1)){
+    if(mapST.get(char1)!=char2) return false;
+}else{
+    mapST.put(char1,char2); // create mapping
+}
+// now to t --> s
+if(mapTS.containsKey(char2)){
+    if(mapTS.get(char2)!=char1) return false;
+}else{
+    mapTS.put(char2,char1); // create mapping
+}
+        }
+        return true;
+        
     }
 }
