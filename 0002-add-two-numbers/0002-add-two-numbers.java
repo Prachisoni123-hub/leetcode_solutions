babbd8ac78d0f4e7fbcdaa9e9636c09d3bbc86aa
Prachisoni123-hub <prachiii0987@gmail.com>
2026-05-23 00:33:07 +0530

    Time: 1 ms (100%), Space: 46.4 MB (75.6%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,10 +10,14 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // created a new node
        ListNode dummy = new ListNode(0);
+       // pointer on it called temp // new nodes ko connect krega
        ListNode temp = dummy;
+       // carry of sum ,initially zero h
        int carry =0;
        while(l1 != null || l2!=null || carry!=0){
+        // eaxh itrration start with carry
          int sum = carry; 
 
          // for l1
@@ -27,8 +31,9 @@ class Solution {
             sum+=l2.val;
             l2=l2.next;
          }
-              // new listnode
+              // new listnode for lastdigit and store it
             temp.next = new ListNode(sum % 10);
+            // calculate carry
              carry = sum / 10;
 
             temp = temp.next;
