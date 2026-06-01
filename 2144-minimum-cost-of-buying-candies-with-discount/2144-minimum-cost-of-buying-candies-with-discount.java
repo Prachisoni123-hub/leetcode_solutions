9bbdf92636840da8b71b5ca02065ddc3fb48705f
Prachisoni123-hub <prachiii0987@gmail.com>
2026-06-01 18:01:10 +0530

    Time: 8 ms (7.82%), Space: 44.5 MB (69.64%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+
+        int ans = 0;
+
+        for (int i = cost.length - 1; i >= 0; i -= 3) {
+            ans += cost[i];          // largest
+            if (i - 1 >= 0) ans += cost[i - 1]; // second largest
+            // i-2 is free
+        }
+
+        return ans;
+    }
+}
