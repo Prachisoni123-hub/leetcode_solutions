6213f3ebdab741117673a7d0181bd2e4777995d2
Prachi Soni <prachiii0987@gmail.com>
2026-06-13 09:01:07 +0530

    Time: 1 ms (88.73%), Space: 43.4 MB (13.82%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,17 +1,18 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-    char[] arr = word.toCharArray();
-      int count = 0;
-
-        for (char ch : arr) {
-            if (ch >= 'A' && ch <= 'Z') {
+        int n = word.length();
+        int count =0;
+        char[] arr = word.toCharArray();
+        for(char ch : arr){
+            if( ch >= 'A' && ch<= 'Z' ){
                 count++;
             }
         }
-           if (count == 0) return true;
-        if (count == arr.length) return true;
-        if (count == 1 && arr[0] >= 'A' && arr[0] <= 'Z') return true;
-
+         if( count ==0) return true;
+            else if(count == n) return true;
+            else if(count == 1 && arr[0] >='A' && arr[0] <='Z') return true;
+        
         return false;
+        
     }
 }
