95d190afd8dab6f2ad1e8004f9f5f7e11823710b
Prachi Soni <prachiii0987@gmail.com>
2026-06-13 08:13:13 +0530

    Time: 241 ms (5.24%), Space: 48 MB (6.93%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,29 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int i = 0, j = s.length() - 1;
+        int n = s.length();
+ // empty string which contains only letters and characters
+ String first = " ";
+ for(int i =0 ;i< n ;i++){
+    char ch = s.charAt(i);
+    if(Character.isLetterOrDigit(ch)){
+        first +=Character.toLowerCase(ch);
+    }
+ }
+  // new string from reverse
+  String reverse = " ";
+  for(int i = n-1 ;i>=0 ;i--){
+    char ch = s.charAt(i);
+      if(Character.isLetterOrDigit(ch)){
+        reverse += Character.toLowerCase(ch);
+      }
 
-        while (i < j) {
-            char left = s.charAt(i);
-            char right = s.charAt(j);
+  }
+  return first.equals(reverse);
 
-            // skip non-alphanumeric
-            if (!Character.isLetterOrDigit(left)) {
-                i++;
-                continue;
-            }
-            if (!Character.isLetterOrDigit(right)) {
-                j--;
-                continue;
-            }
-
-            // compare in lowercase
-            if (Character.toLowerCase(left) != Character.toLowerCase(right)) {
-                return false;
-            }
-
-            i++;
-            j--;
-        }
-        return true;
+        
     }
 }
