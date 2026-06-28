6a4c23cd250ca72b0b6de94d72a0ea4bba2ebfed
Prachi Soni <prachiii0987@gmail.com>
2026-06-28 16:28:13 +0530

    Time: 5 ms (61.79%), Space: 44.6 MB (60.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,35 +1,21 @@
 class Solution {
-  public boolean isAnagram(String s, String t) {
-//         if( s.length()!= t.length()) return false;
+    public boolean isAnagram(String s, String t) {
 
-//         char[] a = s.toCharArray();
-//         char[] b = t.toCharArray();
-//         //sort them
-//         Arrays.sort(a);
-//         Arrays.sort(b);
-//         for( int i =0 ;i<a.length ;i++){
-//             if(a[i]!=b[i]) return false;
-//         }
-//         return true;
-//     }
-// 
+        if( s.length() != t.length()) return false;
 
-        if (s.length() != t.length()) return false;
+        int[] count = new int[26];
 
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
+        for( int i =0 ;i< s.length() ; i++){
+            count[s.charAt(i) - 'a'] ++;
+            count[t.charAt(i) - 'a'] --;
 
-        for (int i = 0; i < s.length(); i++) {
-            char key = s.charAt(i);
-            map1.put(key, map1.getOrDefault(key, 0) + 1);
         }
-
-        for (int i = 0; i < t.length(); i++) {
-            char key = t.charAt(i);
-            map2.put(key, map2.getOrDefault(key, 0) + 1);
+        for( int num : count){
+            if( num != 0){
+                return false;
+            }
         }
-
-        return map1.equals(map2);
+        return true;
+        
     }
 }
-  
