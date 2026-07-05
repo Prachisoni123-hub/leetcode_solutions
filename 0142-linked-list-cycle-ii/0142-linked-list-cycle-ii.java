9fe0eed5bf6f7f7c99afe1e9423caa54dda037a5
Prachi Soni <prachiii0987@gmail.com>
2026-07-05 22:32:38 +0530

    Time: 0 ms (100%), Space: 46.7 MB (49.49%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,22 +11,23 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if (head == null) return null;
+        if(head == null ) return null;
         ListNode slow = head;
         ListNode fast = head;
-    while(fast!= null && fast.next!=null){
-           slow = slow.next;
+        while( fast!=null && fast.next!=null)
+        {
+            slow = slow.next;
             fast = fast.next.next;
-        if(slow == fast){  
-            ListNode temp = head;
-              while (temp != slow){
-                temp = temp.next;
-                slow = slow.next;
-              }
-              return temp;
+            if(slow  == fast){
+                ListNode temp = head;
+                while( temp !=slow){
+                    temp = temp.next;
+                    slow = slow.next;
+                }
+                return temp;
+            }
         }
-    }
-    return null ;
         
+        return null;
     }
 }
