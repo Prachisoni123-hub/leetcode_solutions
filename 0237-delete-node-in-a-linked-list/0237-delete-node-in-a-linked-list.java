31ee956cbbb499db0b69db535e49710e5ee793f9
Prachi Soni <prachiii0987@gmail.com>
2026-07-05 21:40:10 +0530

    Time: 0 ms (100%), Space: 45.5 MB (8.13%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -6,11 +6,11 @@
  *     ListNode(int x) { val = x; }
  * }
  */
- // we dont have to delete the address from memory itself , only chnage the value
 class Solution {
     public void deleteNode(ListNode node) {
-        node.val = node.next.val;
-        node.next= node.next.next;
+       node.val = node.next.val;
+       node.next = node.next.next;
+
         
     }
 }
