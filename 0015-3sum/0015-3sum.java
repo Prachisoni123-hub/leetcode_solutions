17e5262363f71db8ddf5aa323789433189ff1ce0
Prachi Soni <prachiii0987@gmail.com>
2026-07-12 02:03:19 +0530

    Time: 35 ms (44.56%), Space: 59.1 MB (74.21%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,39 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-   List<List<Integer>> list = new ArrayList<>();
-     Arrays.sort(nums);
-     int n = nums.length;
-    
-     for( int i =0;i<n-2;i++){
-        if( i > 0&& nums[i] == nums[i-1]){
-            continue;
-        }
-     int left = i+1;
-     int right = n-1;
-     int target = -nums[i];
-     while( left < right){
-        int sum = nums[left] + nums[right];
+        int n = nums.length;
+        if( n < 3) return new ArrayList<>();
+        Arrays.sort(nums);
 
-        if (sum == target){
-            list.add(Arrays.asList(nums[i] ,nums[left] ,nums[right]));
-            left++;
-            right--;
+        List<List<Integer>> ans = new ArrayList<>();
+         // loop for fix an element : n1
+         for( int k =0;k<n-2;k++){
+            if( k >0 && nums[k] == nums[k-1])continue;
+         
 
-            while( left < right && nums[left] == nums[left-1]){
-                left++;
-            }
-            while(left < right && nums[right] == nums[right+1]){
-                right--;
-            }
-        }
-          else if( sum < target) left++;
+         // now make target
+         int n1 = nums[k];
+         int target = -n1;
 
-          else right--;
-        }
-     }
-     
-     return list;
-     
+         // now two sum
+         
+         int i =k+1;
+         int j = n-1;
+         while(i<j){
 
+         int sum = nums[i]+nums[j];
+         if( sum == target) {
+            ans.add(Arrays.asList(nums[k], nums[i] , nums[j]));i++;
+            j--;
+            // check for duplicates
+         while( i<j && nums[i] == nums[i-1])i++;
+         if(i<j && nums[j] == nums[j+1]) j--;
+
+         } else if( sum > target) j--;
+         else i++;
+         }
+    }
+        return ans;
     }
 }
