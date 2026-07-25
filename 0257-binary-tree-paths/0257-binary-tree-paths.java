db7a530833ca822f9fe67aefea8a5f472fa77f9b
Prachi Soni <prachiii0987@gmail.com>
2026-07-25 17:31:48 +0530

    Time: 4 ms (73.55%), Space: 44 MB (78.88%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -14,20 +14,24 @@
  * }
  */
 class Solution {
-     public void path(TreeNode root ,String s, List<String> ans ){
-        if(root == null) return;
-  if( root.left == null && root.right == null){
-          s+=root.val;
-          ans.add(s);
-          return;
-  }
-  path(root.left, s+ root.val + "->",ans);
-  path(root.right, s+ root.val + "->",ans);
-        }
-    public List<String> binaryTreePaths(TreeNode root) {
-    List<String> ans = new ArrayList<>();
-    path(root, "" , ans);
-    return ans;
+    public void path (TreeNode root , String s, List<String>ans)
+    {
+
+        if( root==null) return;
+          if( root.left == null && root.right == null){
+           s +=root.val;
+            ans.add(s);
+            return;
+       }
+            path(root.left,s+root.val+"->" ,ans);
+              path(root.right,s+root.val+"->",ans);
+          }
+    
+          public List<String> binaryTreePaths(TreeNode root) {
+    
+        List<String> ans = new ArrayList<>();
+             path(root,"",ans);
+          return ans;
         
     }
 }
