bba1d94e265b86043229757f75cba0731a4d62d6
Prachi Soni <prachiii0987@gmail.com>
2026-08-19 21:28:45 +0530

    Time: 0 ms (100%), Space: 42.6 MB (87.5%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -1,27 +1,26 @@
 class Solution {
     public String breakPalindrome(String str) {
-       
-        char ch[] = str.toCharArray();
-         int n  = ch.length;
 
-          // Single character palindrome ko break nahi kar sakte
-        if (n == 1) {
-            return "";
-        }
+        char ch[] = str.toCharArray();
+        int n = ch.length;
+
+      if( n == 1)
+      {
+          return "";
+      }        
 
         for( int i=0;i<n/2;i++)
         {
-            if(ch[i] != 'a')
+            if(ch[i]  != 'a')
             {
-                ch[i] = 'a';
+                ch[i] ='a';
                 return new String(ch);
             }
         }
-        // or agar sare characters 'a' hi milrhe h to iss loop s bahar agye
-        // so hum last wale character ko 'b' bnadenge
-        ch[n-1] = 'b';
-        
-        return new String(ch);
+       
+       ch[n-1] = 'b';
+       return new String(ch);
+
         
     }
 }
