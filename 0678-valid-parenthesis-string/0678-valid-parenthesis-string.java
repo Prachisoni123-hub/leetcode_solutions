75596c75ceb389fd1ef9f9a8170d5ca374a73397
Prachi Soni <prachiii0987@gmail.com>
2026-08-20 19:55:29 +0530

    Time: 0 ms (100%), Space: 42.7 MB (68.9%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkValidString(String s) {
+
+        int high =0; // for opening bracket
+        int low=0;  // for closing bracket
+
+        for( char ch : s.toCharArray())
+        {
+            if( ch  == '(')
+            {
+                low++;
+                high++;
+
+            }else if ( ch == ')')
+            {
+                low--;
+                high--;
+            }else{
+                // '*'
+                low--;
+                high++;
+            }
+
+            if(high <0)
+            return false;
+        
+
+  low = Math.max(0,low);
+        }
+        return low ==0;
+    }
+}
