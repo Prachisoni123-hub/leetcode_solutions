88e75c8b433fd72a0194d99463acb07eee267003
Prachi Soni <prachiii0987@gmail.com>
2026-09-11 12:44:46 +0530

    Time: 18 ms (38.9%), Space: 45.5 MB (66.47%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,26 +1,56 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int n = nums.length;
+        // approch 1
+    //     int n = nums.length;
 
+    //     Arrays.sort(nums);
+
+    //     int closest = nums[0]+nums[1]+nums[2];
+
+    //     for( int i=0;i<n;i++)
+    //     {
+    //         for( int j = i+1 ; j<n;j++)
+    //         {
+    //             for( int k= j+1 ; k<n;k++)
+    //             {
+    //                 int sum = nums[i]+nums[j]+nums[k];
+    //                 if( Math.abs(sum-target) < Math.abs(closest - target))
+    //                 {
+    //                     closest = sum;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return closest;
+        
+
+
+        // approch 2
+        int n = nums.length;
+        
         Arrays.sort(nums);
 
-        int closest = nums[0]+nums[1]+nums[2];
+        int closestsum = nums[0]+nums[1]+nums[2];
 
-        for( int i=0;i<n;i++)
+          for(int  k =0;k<n-2;  k++)
         {
-            for( int j = i+1 ; j<n;j++)
+            int i = k+1;
+            int j = n-1;
+
+            while( i <j)
             {
-                for( int k= j+1 ; k<n;k++)
-                {
-                    int sum = nums[i]+nums[j]+nums[k];
-                    if( Math.abs(sum-target) < Math.abs(closest - target))
-                    {
-                        closest = sum;
-                    }
-                }
-            }
+                int sum = nums[k] +nums[i]+nums[j]; 
+
+   if(Math.abs(sum-target) < Math.abs( closestsum - target))
+   {
+    closestsum = sum;
+   }
+   if( sum == target) return sum;
+
+                if( sum < target ) i++;
+                if( sum> target) j--;
+                       }
         }
-        return closest;
-        
-    }
+        return closestsum;
+     }
 }
